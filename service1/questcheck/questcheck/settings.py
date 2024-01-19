7f1126bb99207ffb146b344103e1b6ae193b0e43
ctpanh <ctpanh03@gmail.com>
2024-01-19 10:24:43 +0700

    change file

--- service1/questcheck/questcheck/settings.py
+++ service1/questcheck/questcheck/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         "ENGINE": "django.db.backends.postgresql",
         "NAME": "test",
         "USER": "postgres",
-        "PASSWORD": "1",
+        "PASSWORD": "12345678",
         "HOST": "localhost",
         "PORT": "5432",
     }
